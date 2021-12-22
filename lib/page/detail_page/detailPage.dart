746bbdd7bacc0401a998284d9136c8e6f550ce28
danhan <danhandev@gmail.com>
2021-12-22 22:06:31 +0900

    Feat: Add book_mark, location_pin button

--- lib/page/detail_page/detailPage.dart
+++ lib/page/detail_page/detailPage.dart
@@ -1,7 +1,8 @@
-import 'package:cafegation/constants/colors.dart';
+// import 'package:cafegation/constants/colors.dart';
 import 'package:flutter/material.dart';
+
 class detailPage extends StatefulWidget {
-  const detailPage({ Key? key }) : super(key: key);
+  const detailPage({Key? key}) : super(key: key);
 
   @override
   _detailPageState createState() => _detailPageState();
@@ -11,11 +12,38 @@ class _detailPageState extends State<detailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kMainColor,
-        shadowColor: kGreyColor,
-        title: Text('detail Page'),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.bookmark_outline),
+              onPressed: () {
+                print('book mark button is pressed');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.location_pin),
+              onPressed: () {
+                print('location_pin button is pressed');
+              },
+            ),
+          ],
         ),
-    );
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/sample_image.jpg'),
+            ),
+          ),
+        ));
   }
 }
