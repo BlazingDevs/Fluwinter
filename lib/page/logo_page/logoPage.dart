00f437b92ff99c1cb3c8609f354957b22b043055
Cho Sangwook <chosw1002@naver.com>
2021-12-23 00:04:02 +0900

    Move on to search page without click

--- lib/page/logo_page/logoPage.dart
+++ lib/page/logo_page/logoPage.dart
@@ -9,68 +9,27 @@ class logoPage extends StatefulWidget {
 
 class _logoPageState extends State<logoPage> {
   @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => searchPage()));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF424242),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => searchPage()));
-                },
-              ),
-              flex: 3,
-            ),
-            Expanded(
-              child: InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => searchPage()));
-                  },
-                  child: Container(
-                    width: 2500,
-                    child: Text(
-                      'Cafégation',
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(color: Color(0xFFef39a9), fontSize: 50.0),
-                    ),
-                  )),
-              flex: 1,
-            ),
-            Expanded(
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => searchPage()));
-                },
-                child: Container(
-                  width: 2500,
-                  child: Text(
-                    '클릭하여 다음으로 이동',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Color(0xFFe797c8), fontSize: 25.0),
-                  ),
-                ),
-              ),
-              flex: 1,
-            ),
-            Expanded(
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => searchPage()));
-                },
-              ),
-              flex: 3,
-            )
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Colors.grey[850],
+        body: Center(
+            child: Container(
+          child: Text(
+            "카페게이션",
+            style: TextStyle(fontSize: 45.0, color: Colors.pinkAccent),
+          ),
+        )));
   }
 }
