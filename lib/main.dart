34da81d3f9df51be9c9076c7850097a910a81f60
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2021-12-28 00:30:08 +0900

    fix: connect with firebase as chrome web app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:cafegation/page/logo_page/logoPage.dart';
 import 'package:flutter/material.dart';
-import 'constants/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDi9VS9_ovimWTHSxamGDycVMLM_mGtvNY",
+      authDomain: "cafegation.firebaseapp.com",
+      projectId: "cafegation",
+      storageBucket: "cafegation.appspot.com",
+      messagingSenderId: "898555262184",
+      appId: "1:898555262184:web:587451332320a73857455d"
+    )
+  );
   runApp(const MyApp());
 }
 
