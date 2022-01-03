a7053c4a2f7cc15683650a1a2775ce2cc65c4f7c
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2022-01-03 23:33:40 +0900

    fix: add platform detection for firebase init isssue

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:cafegation/page/logo_page/logoPage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,16 +6,18 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Initialize Firebase
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyDi9VS9_ovimWTHSxamGDycVMLM_mGtvNY",
-      authDomain: "cafegation.firebaseapp.com",
-      projectId: "cafegation",
-      storageBucket: "cafegation.appspot.com",
-      messagingSenderId: "898555262184",
-      appId: "1:898555262184:web:587451332320a73857455d"
-    )
-  );
+  if(!(Platform.isAndroid || Platform.isIOS)){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDi9VS9_ovimWTHSxamGDycVMLM_mGtvNY",
+        authDomain: "cafegation.firebaseapp.com",
+        projectId: "cafegation",
+        storageBucket: "cafegation.appspot.com",
+        messagingSenderId: "898555262184",
+        appId: "1:898555262184:web:587451332320a73857455d"
+      )
+    );
+  }
   runApp(const MyApp());
 }
 
