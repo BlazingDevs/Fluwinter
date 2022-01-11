9b4961775a17d9238667898936ce8e41f13078ab
Cho Sangwook <chosw1002@naver.com>
2022-01-11 17:16:29 +0900

    now linked with firestore

--- lib/page/list_page/listPage.dart
+++ lib/page/list_page/listPage.dart
@@ -1,6 +1,8 @@
 import 'package:cafegation/constants/colors.dart';
 import 'package:cafegation/page/detail_page/detailPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class listPage extends StatefulWidget {
   const listPage({Key? key}) : super(key: key);
@@ -10,61 +12,44 @@ class listPage extends StatefulWidget {
 }
 
 class _listPageState extends State<listPage> {
+  List<Widget> makeListWidget(AsyncSnapshot snapshot) {
+    return snapshot.data.docs.map<Widget>((document) {
+      return ListTile(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => detailPage()),
+          );
+        },
+        title: Text(document["name"]),
+        subtitle: Text(document["location"]),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFF7B938),
+        backgroundColor: Colors.blue[200],
         shadowColor: kGreyColor,
         title: Text('list Page'),
       ),
-      body: Stack(children: <Widget>[
-        SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.all(17.0),
-          child: ListView.separated(
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return ListTile(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => detailPage()),
-                  );
-                },
-                title: Text(
-                  'CAFENAMU',
-                  style: TextStyle(fontSize: 22.0),
-                ),
-                subtitle: Text(
-                  "  08:00 ~ 22:00\n  홍익대학교 정문 L층",
-                  style: TextStyle(fontSize: 14.0),
-                ),
-                leading: ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)),
-                    child: Image(
-                      image: NetworkImage(
-                          'https://coffee.dankook.ac.kr/html_portlet_repositories/images/ExtImgFile/10158/1766093/1952287/42012.png'),
-                    )),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 5.0, horizontal: 0.0),
-                trailing: Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                  size: 25.0,
-                ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return Divider();
-            },
-          ),
-        )),
-      ]),
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("cafes").snapshots(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              default:
+                return ListView(
+                  children: makeListWidget(snapshot),
+                );
+            }
+          },
+        ),
+      ),
     );
   }
 }
