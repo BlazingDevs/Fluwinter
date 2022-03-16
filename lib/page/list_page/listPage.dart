58e15a783bee44bb74b206430623b40b10f1d6e3
Cho Sangwook <chosw1002@naver.com>
2022-03-16 22:25:50 +0900

    list_page is now linked to firestore user_data

--- lib/page/list_page/listPage.dart
+++ lib/page/list_page/listPage.dart
@@ -1,5 +1,6 @@
 import 'package:cafegation/constants/colors.dart';
 import 'package:cafegation/page/detail_page/detailPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -12,7 +13,11 @@ class listPage extends StatefulWidget {
 }
 
 class _listPageState extends State<listPage> {
-  bool favorite_pressed = true;
+  bool _favoriteButtonPressed = false;
+
+  DocumentReference _documentReference = FirebaseFirestore.instance
+      .collection('user_data')
+      .doc('hpRMlobAuTPg5lOjC3zTnJJCeHa2');
   List<Widget> makeListWidget(AsyncSnapshot snapshot) {
     return snapshot.data.docs.map<Widget>((document) {
       return ListTile(
@@ -34,13 +39,19 @@ class _listPageState extends State<listPage> {
           title: Text(document["name"]),
           subtitle: Text(document["location"]),
           trailing: IconButton(
-            icon:
-                Icon(favorite_pressed ? Icons.favorite : Icons.favorite_border),
+            icon: Icon(_favoriteButtonPressed
+                ? Icons.favorite
+                : Icons.favorite_border),
             color: Colors.red,
             iconSize: 25.0,
             onPressed: () {
+              //if문으로 favorite_pressed의 t/f여부를 보고 t면 f로, f면 t로 바꾸기
+              //t였다면 userdata에서 삭제, f였다면 userdata에 추가
               setState(() {
-                favorite_pressed = !favorite_pressed;
+                _favoriteButtonPressed = !_favoriteButtonPressed;
+                _documentReference.set({
+                  'favorites': document["name"]
+                });
               });
             },
           ));
