49e5a5fcf718d26059e1d5a2a0bfa7c483c444d8
Jeon-jisu <wltnjeon0119@gmail.com>
2022-03-16 23:33:31 +0900

    Add Android ver + iOS ver Google map

---
+++ lib/map/google_map.dart
@@ -0,0 +1,2 @@
+//Api key 
+//AIzaSyBjFSgXJzOyTK8vfna_jjM8scq9s21cRlg
--- lib/page/map_page/mapPage.dart
+++ lib/page/map_page/mapPage.dart
@@ -1,281 +1,70 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:kakao_map_flutter/kakao_map_flutter.dart';
-
-// void main() => runApp(KakaoMapTest());
-
-// class KakaoMapTest extends StatelessWidget {
-//   KakaoMapTest({Key? key}) : super(key: key);
-
-//   late final KakaoMapController _mapController;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//           body: SafeArea(
-//         child: Column(children: [
-//           Expanded(
-//               child: KakaoMap(
-//             initLocation: KakaoLatLng(33.450701, 126.570667),
-//             kakaoApiKey: "258df8a7062d2ae5f0006f6e0b6796a9",
-//             clustererServiceEnable: true,
-//             onMapCreated: (controller) {
-//               _mapController = controller;
-//             },
-//             onMapLoaded: () {
-//               Get.rawSnackbar(
-//                   message: "맵 로드 완료",
-//                   margin: const EdgeInsets.all(8),
-//                   borderRadius: 12.0,
-//                   snackPosition: SnackPosition.TOP);
-//               _mapController.setNowLocation();
-//             },
-//             onMarkerTouched: (markerLocation, markerIndex) {
-//               Get.rawSnackbar(
-//                   message: "$markerLocation, $markerIndex",
-//                   margin: const EdgeInsets.all(8),
-//                   borderRadius: 12.0,
-//                   snackPosition: SnackPosition.TOP);
-//             },
-//           )),
-//           _customButton("현재 위치로 이동하고 커스텀 오버레이 추가하기", onTap: () async {
-//             final location = await _mapController.setNowLocation();
-//             if (location != null) {
-//               _mapController.deleteAllCustomOverlays();
-//               _mapController.addCustomOverlay(location);
-//             }
-//           }),
-//           _customButton("지도 위치에 마커 추가하기", onTap: () async {
-//             _mapController.addMarker(await _mapController.getCenter());
-//           }, color: Colors.green),
-//           _customButton("모든 마커 삭제하기", onTap: () {
-//             _mapController.deleteAllMarkers();
-//           }, color: Colors.red),
-//           _customButton("모든 마커 클러스터링하기", onTap: () {
-//             if (!_mapController.nowClusteringEnabled())
-//               _mapController.startClustering(minLevel: 5);
-//             else
-//               _mapController.updateClustering();
-//           }, color: Colors.black87),
-//         ]),
-//       )),
-//     );
-//   }
-
-//   Widget _customButton(String text,
-//           {Function()? onTap, Color color = Colors.lightBlue}) =>
-//       SizedBox(
-//           width: double.infinity,
-//           child: Material(
-//             color: color,
-//             child: InkWell(
-//                 onTap: onTap,
-//                 child: Padding(
-//                     padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
-//                     child: Text(text,
-//                         style: TextStyle(
-//                             color: Colors.white, fontWeight: FontWeight.bold),
-//                         textAlign: TextAlign.center))),
-//           ));
-// }
 import 'package:cafegation/constants/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:kakaomap_webview/kakaomap_webview.dart';
-//02.02(수) push 기준 쓸데없는 페이지임. 02.28 기준 안 쓸데없음.
-const String kakaoMapKey = '258df8a7062d2ae5f0006f6e0b6796a9';
-var markers = ["피오니", "라헬의 부엌"];
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(MaterialApp(home: KakaoMapTest()));
+  runApp(KakaoMapTest());
 }
 
 class KakaoMapTest extends StatelessWidget {
+  const KakaoMapTest({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    var _mapController;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: Colors.blue[200],
-        title: Text('내 주변 카페 찾기')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          KakaoMapView(
-              width: size.width,
-              height: 650,
-              kakaoMapKey: kakaoMapKey,
-              lat:37.552590921387676, 
-              lng:126.9248596640318,
-              showMapTypeControl: true,
-              showZoomControl: true,
-              draggableMarker: false,
-              mapType: MapType.BICYCLE,
-              mapController: (controller) {
-                _mapController = controller;
-              },
-              polygon: KakaoPolygon(
-                  polygon: [
-                    KakaoLatLng(37.54437685093866, 126.92296870883536),
-                    KakaoLatLng(37.54717721045083, 126.90873476390736),
-                    KakaoLatLng(37.54717721045083, 126.90873476390736),
-                    KakaoLatLng(37.562901433668124, 126.92638442980359),
-                    KakaoLatLng(37.55505425341539, 126.93678586780096),
-                    KakaoLatLng(37.54415008748556, 126.92946099846323)
-                  ],
-                  polygonColor: Colors.red,
-                  polygonColorOpacity: 0,
-                  strokeColor: Colors.black,
-                  strokeWeight: 2.5,
-                  strokeColorOpacity: 0.9),
-              overlayText: '홍익대학교',
-              customOverlayStyle: '''<style>
-              .customoverlay {position:relative;bottom:85px;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;float:left;}
-.customoverlay:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
-.customoverlay a {display:block;text-decoration:none;color:#000;text-align:center;border-radius:6px;font-size:14px;font-weight:bold;overflow:hidden;background: #d95050;background: #d95050 url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center;}
-.customoverlay .title {display:block;text-align:center;background:#fff;margin-right:35px;padding:10px 15px;font-size:14px;font-weight:bold;}
-.customoverlay:after {content:'';position:absolute;margin-left:-12px;left:50%;bottom:-12px;width:22px;height:12px;background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
-              </style>''',
-              customOverlay: '''
-var content = '<div class="customoverlay">' +
-    '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-    '    <span class="title">카카오!</span>' +
-    '  </a>' +
-    '</div>';
-
-var position = new kakao.maps.LatLng(33.450701, 126.570667);
-
-var customOverlay = new kakao.maps.CustomOverlay({
-    map: map,
-    position: position,
-    content: content,
-    yAnchor: 1
-});
-              ''',
-              markerImageURL:
-                  'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-              onTapMarker: (message) {
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(SnackBar(content: Text(message.message)));
-              },
-              zoomChanged: (message) {
-                debugPrint('current zoom level : ${message.message}');
-              },
-              cameraIdle: (message) {
-                KakaoMapUtil util = KakaoMapUtil();
-                KakaoLatLng latlng = util.getLatLng(message.message);
-                debugPrint('current lat lng : ${latlng.lat}, ${latlng.lng}');
-              }),
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                            shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(10.0),
-                            ),
-                            primary: Colors.blue[200],
-                            onPrimary: Colors.black),
-              child: Text('kakao map open'),
-              onPressed: () async {
-                await _openKakaoMapScreen(context);
-              })
-        ],
+        return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-  
-  Future<void> _openKakaoMapScreen(BuildContext context) async {
-    KakaoMapUtil util = KakaoMapUtil();
-
-    // String url = await util.getResolvedLink(
-    //     util.getKakaoMapURL(37.402056, 127.108212, name: 'Kakao 본사'));
-
-    /// This is short form of the above comment
-    String url =
-        await util.getMapScreenURL(37.5515814, 126.9227864, name: '홍익대학교');
-
-    debugPrint('url : $url');
-
-    Navigator.push(
-        context, MaterialPageRoute(builder: (_) => KakaoMapScreen(url: url)));
-  }
-
-  Widget _testingCustomScript(
-      {required Size size, required BuildContext context}) {
-        
-    return KakaoMapView(
-        width: size.width,
-        height: 400,
-        kakaoMapKey: kakaoMapKey,
-        lat: 37.5515814,
-        lng: 126.9227864,
-        customScript: '''  
-        
-        var markers = ["피오니", "라헬의 부엌"];
-    
-    function addMarker(position) {
-    
-      var marker = new kakao.maps.Marker({position: position});
-
-      marker.setMap(map);
-    
-      markers.push(marker);
-    }
-    
-    kakao.maps.event.addListener(markers[0], 'click', function(){ 
-      onTapMarker.postMessage('marker 0 is tapped'); }); 
-    kakao.maps.event.addListener(markers[1], 'click', function(){ 
-      onTapMarker.postMessage('marker 1 is tapped'); }); 
-    kakao.maps.event.addListener(markers[2], 'click', function(){ 
-      onTapMarker.postMessage('marker 2 is tapped'); });
-
-    
-		  var zoomControl = new kakao.maps.ZoomControl();
-      map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-   
-      var mapTypeControl = new kakao.maps.MapTypeControl();
-      map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-        onTapMarker: (message) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(message.message)));
-        }''',
+      home: MapSample(),
     );
   }
 }
+  
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
 
-// class mapPage extends StatefulWidget {
-//   const mapPage({Key? key}) : super(key: key);
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
 
-//   @override
-//   _mapPageState createState() => _mapPageState();
-// }
+  // 초기 카메라 위치
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.551769, 126.924954),
+    zoom: 15,
+  );
 
-// class _mapPageState extends State<mapPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         elevation: 0.0,
-//         leading: IconButton(
-//           icon: Icon(Icons.arrow_back_ios),
-//           onPressed: () {
-//             Navigator.pop(context);
-//           },
-//         ),
-//         backgroundColor: Colors.transparent,
-//         shadowColor: kGreyColor,
-//         title: Text('Map Page'),
-//         actions: [
-//           IconButton(
-//               icon: Icon(Icons.add_location),
-//               onPressed: () {
-//                 print("location button is pressed");
-//               })
-//         ],
-//       ),
-//     );
-//   }
-// }
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+    @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex, // 초기 카메라 위치
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+
+      // floatingActionButton을 누르게 되면 _goToTheLake 실행된다.
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+} 
