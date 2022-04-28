9c2f595b0e22769ddfa28e477fc9792045d95f37
Cho Sangwook <chosw1002@naver.com>
2022-04-28 15:02:02 +0900

    favorite buttons work individually

--- lib/page/list_page/listPage.dart
+++ lib/page/list_page/listPage.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:favorite_button/favorite_button.dart';
 
 class listPage extends StatefulWidget {
   const listPage({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class listPage extends StatefulWidget {
 
 class _listPageState extends State<listPage> {
   bool _favoriteButtonPressed = false;
-  int i = 0;
+  int idx = 0;
   FirebaseAuth user = FirebaseAuth.instance;
   List<Widget> makeListWidget(AsyncSnapshot snapshot) {
     return snapshot.data.docs.map<Widget>((document) {
@@ -36,25 +37,16 @@ class _listPageState extends State<listPage> {
           ),
           title: Text(document["name"]),
           subtitle: Text(document["location"]),
-          trailing: IconButton(
-            icon: Icon(_favoriteButtonPressed
-                ? Icons.favorite
-                : Icons.favorite_border),
-            color: Colors.red,
+          trailing: FavoriteButton(
             iconSize: 25.0,
-            onPressed: () {
-              //if문으로 favorite_pressed의 t/f여부를 보고 t면 f로, f면 t로 바꾸기
-              //t였다면 userdata에서 삭제, f였다면 userdata에 추가
-              setState(() {
-                _favoriteButtonPressed = !_favoriteButtonPressed;
-                FirebaseFirestore.instance
-                    .collection('user_data')
-                    .doc(user.currentUser!.uid)
-                    .set({
-                  'favorites': {i.toString(): document["name"]}
-                }, SetOptions(merge: true));
-              });
-              i += 1;
+            isFavorite: false,
+            valueChanged: (_isFavorite) {
+              var list = [document["name"]];
+              FirebaseFirestore.instance
+                  .collection('user_data')
+                  .doc(user.currentUser!.uid)
+                  .set({"favorites": FieldValue.arrayUnion(list)},
+                      SetOptions(merge: true));
             },
           ));
     }).toList();
