a6fb6e648e1ab4e4b957dd254a40ead9656321a2
Cho Sangwook <chosw1002@naver.com>
2022-09-06 14:50:27 +0900

    ♻️ REFACTOR: Change stateless to stateful

--- lib/page/list_page/CafeItem.dart
+++ lib/page/list_page/CafeItem.dart
@@ -7,10 +7,15 @@ import 'package:cafegation/page/detail_page/detailPage.dart';
 import 'package:cafegation/services/database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class CafeItem extends StatelessWidget {
+class CafeItem extends StatefulWidget {
   final Cafe cafe;
-  CafeItem({required this.cafe, Key? key}) : super(key: key);
+  const CafeItem({required this.cafe, Key? key}) : super(key: key);
 
+  @override
+  State<CafeItem> createState() => _CafeItemState();
+}
+
+class _CafeItemState extends State<CafeItem> {
   @override
   bool state = false;
   List lst = [];
@@ -21,39 +26,41 @@ class CafeItem extends StatelessWidget {
             context,
             MaterialPageRoute(
                 builder: (context) => detailPage(
-                      cafeName: cafe.id,
+                      cafeName: widget.cafe.id,
                     )),
           );
         },
         leading: ClipRRect(
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
           child: Image(
-            image: NetworkImage(cafe.images, scale: 3.0),
+            image: NetworkImage(widget.cafe.images, scale: 3.0),
           ),
         ),
-        title: Text(cafe.name),
-        subtitle: Text(cafe.location),
+        title: Text(widget.cafe.name),
+        subtitle: Text(widget.cafe.location),
         trailing: IconButton(
           icon: Icon(stateBool(AuthService().user!) == true ? Icons.favorite : Icons.favorite_border),
           color: Colors.red,
           iconSize: 25.0,
           onPressed: () {
-            var list = [
-              {"id": cafe.id, "tag": cafe.tags}
-            ];
-            if (state == false) {
-              FirebaseFirestore.instance
-                  .collection('user_data')
-                  .doc(FirebaseAuth.instance.currentUser!.uid)
-                  .set({"favorites": FieldValue.arrayUnion(list)}, SetOptions(merge: true));
-              state = true;
-            } else {
-              FirebaseFirestore.instance
-                  .collection('user_data')
-                  .doc(FirebaseAuth.instance.currentUser!.uid)
-                  .set({"favorites": FieldValue.arrayRemove(list)}, SetOptions(merge: true));
-              state = false;
-            }
+            setState(() {
+              var list = [
+                {"id": widget.cafe.id, "tag": widget.cafe.tags}
+              ];
+              if (state == true) {
+                state = false;
+                FirebaseFirestore.instance
+                    .collection('user_data')
+                    .doc(FirebaseAuth.instance.currentUser!.uid)
+                    .set({"favorites": FieldValue.arrayRemove(list)}, SetOptions(merge: true));
+              } else {
+                state = true;
+                FirebaseFirestore.instance
+                    .collection('user_data')
+                    .doc(FirebaseAuth.instance.currentUser!.uid)
+                    .set({"favorites": FieldValue.arrayUnion(list)}, SetOptions(merge: true));
+              }
+            });
           },
         ));
   }
@@ -62,7 +69,7 @@ class CafeItem extends StatelessWidget {
     final a = listMaker();
     return a.then((value) {
       print(value);
-      if (value.contains(cafe.id)) {
+      if (value.contains(widget.cafe.id)) {
         print('true');
         state = true;
       } else {
@@ -70,63 +77,6 @@ class CafeItem extends StatelessWidget {
       }
       return state;
     });
-
-    // print('next123');
-    // print(state);
-    // return state;
-
-    // a.get().then(
-    //   (value) {
-    //     temp = value.data();
-    //     temp = temp['favorites'];
-    //     for (var d in temp) {
-    //       d = d['id'];
-    //       print(d);
-    //       if (d == cafe.id) {
-    //         state = true;
-    //         print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-    //         return state;
-    //       } else {
-    //         print("doesn't fit");
-    //       }
-    //     }
-    //   },
-    // );
-    // if (DataBaseService().userFavorite(AuthService().user!.uid) == cafe.id) {
-    //   state = true;
-    // }
-    //final dbcall = await DataBaseService().userFavorite(AuthService().user!.toString());
-    // final db = FirebaseFirestore.instance.collection('user_data').doc(AuthService().user!.toString()).get().then((value) {
-    //   final data = value.data() as Map<String, dynamic>;
-    //   print(data.values.first);
-    //   print('123');
-    //   // a.add(value);
-    // });
-
-    // var db = await FirebaseFirestore.instance.collection('user_data').doc(AuthService().user!.toString()).get();
-    // print(db.data());
-    // print('object');
-    // print(state);
-    // print('listMaker function working');
-    // for (String a in dbcall) {
-    //   print('321321');
-    //   print(a);
-    //   print(cafe.id);
-    //   print('printed');
-    //   if (a == cafe.id) {
-    //     state = true;
-    //     break;
-    //   }
-    // }
-
-    // return docs.get().then((DocumentSnapshot doc) {
-    //   final data = doc.data() as Map<String, dynamic>;
-    //   print(data.values.first);
-    //   print('clear');
-    //   return state;
-    // });
-    // print(state);
-    // print(33);
   }
 
   Future<List<dynamic>> listMaker() {
@@ -150,8 +100,3 @@ class CafeItem extends StatelessWidget {
     // return c;
   }
 }
-
-
-//TODO: listMaker랑 userFavorite을 https://firebase.google.com/docs/firestore/query-data/get-data?hl=ko#get_a_document에 기반하여 수정하기 => done
-
-//TODO: _castError {type 'Null' is not a subtype of type 'Map<String, dynamic>' in type cast} 해결하기
